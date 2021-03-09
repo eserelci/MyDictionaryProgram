2616a5cd0034b4364d14f32692870bb490e4c654
Eser <47974991+eserelci@users.noreply.github.com>
2021-03-10 00:43:24 +0300

    Proje dosyası ekle.

---
+++ MyDictionaryProgram/Dictionary.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyDictionaryProgram
+{
+    public class Dictionary<Key,Value>
+    {
+        Key[] monthNumberArray;
+        Value[] monthNameArray;
+        public Dictionary()
+        {           
+            monthNumberArray = new Key[0];
+            monthNameArray = new Value[0];
+        }
+        public void Add(Key number, Value month)
+        {
+            Key[] tempMonthNumberArray = monthNumberArray;
+            monthNumberArray = new Key[monthNumberArray.Length + 1];
+            for (int i = 0; i < tempMonthNumberArray.Length; i++)
+            {
+                monthNumberArray[i] = tempMonthNumberArray[i];
+            }
+            monthNumberArray[monthNumberArray.Length - 1] = number;
+            Value[] tempMonthNameArray = monthNameArray;
+            monthNameArray = new Value[monthNameArray.Length + 1];
+            for (int i = 0; i < tempMonthNameArray.Length; i++)
+            {
+                monthNameArray[i] = tempMonthNameArray[i];
+            }
+            monthNameArray[monthNameArray.Length - 1] = month;
+        }
+        public void Listele()
+        {
+            for (int i = 0; i < monthNumberArray.Length; i++)
+            {
+                Console.WriteLine("Month Name: "+monthNameArray[i]+" Month Number: "+monthNumberArray[i]);
+            }
+        }
+    }
+}
---
+++ MyDictionaryProgram/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MyDictionaryProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<int, string> Months = new Dictionary<int, string>();
+            Months.Add(1, "January");
+            Months.Add(2, "February");
+            Months.Add(3, "March");
+            Months.Add(4, "April");
+            Months.Add(5, "May");
+            Months.Add(6, "June");
+            Months.Add(7, "July");
+            Months.Add(8, "August");
+            Months.Add(9, "September");
+            Months.Add(10, "October");
+            Months.Add(11, "November");
+            Months.Add(12, "December");
+            Months.Listele();
+
+        }
+    }
+}
